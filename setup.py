fb08b1613b74164d382f0779ec848a546c5ff501
Harold J. Iwen <52970935+Hiwen-STEM@users.noreply.github.com>
2021-05-24 21:01:41 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'numpy'
     ],
     classifiers=[
-        'Development Status :: 3 - Beta',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
